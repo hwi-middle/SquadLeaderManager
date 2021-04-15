b4a827dbfc40a150f79dbdb3a2900193d3f6ac85
hwi-middle <gnlwnd5084@gmail.com>
2021-04-15 23:49:41 +0900

    첫 세팅 씬 일부 레이아웃 및 안내 텍스트 문체 변경

--- Assets/_MyAssets/Scripts/InitSettingsSceneController.cs
+++ Assets/_MyAssets/Scripts/InitSettingsSceneController.cs
@@ -7,7 +7,6 @@ public class InitSettingsSceneController : MonoBehaviour
 {
     public Text TitleText;
     public Text DescriptionText;
-    public Text TempRank;
     public InputField TextInputField;
     public Button PositiveButton;
     public Button NegativeButton;
@@ -49,45 +48,43 @@ public class InitSettingsSceneController : MonoBehaviour
             case 0:
                 TextInputField.gameObject.SetActive(false);
                 NegativeButton.gameObject.SetActive(false);
-                TempRank.gameObject.SetActive(false);
 
-                TitleText.text = "충성!";
+                TitleText.text = "안녕하세요!";
                 DescriptionText.text = "지금부터 초기 설정을 시작하겠습니다.";
-                PositiveButtonText.text = "어, 그래.";
+                PositiveButtonText.text = "다음";
                 break;
             case 1:
                 TextInputField.gameObject.SetActive(true);
                 NegativeButton.gameObject.SetActive(true);
-                TempRank.gameObject.SetActive(true);
 
-                TitleText.text = "관등성명 여쭤봐도 되겠습니까?";
-                DescriptionText.text = "아래 창에 입력해주십쇼.";
-                PositiveButtonText.text = "여기";
-                NegativeButtonText.text = "인사 다시해봐";
+                TitleText.text = "성함을 알려주세요.";
+                DescriptionText.text = "4글자까지 입력할 수 있습니다.";
+                PositiveButtonText.text = "확인";
+                NegativeButtonText.text = "뒤로";
                 break;
             case 2:
                 string name = TextInputField.text;
+                PlayerPrefs.SetString("UserName", name);
                 if (name == "")
                 {
-                    TitleText.text = "잘 못 들었습니다?";
-                    DescriptionText.text = "어, 여기 입력칸 비우시면 안됩니다.";
+                    TitleText.text = "다시 입력해주세요.";
+                    DescriptionText.text = "이름은 비워둘 수 없습니다.";
                     DialogPage--;
                     break;
                 }
 
-                TempRank.gameObject.SetActive(false);
                 TextInputField.gameObject.SetActive(false);
                 TitleText.text = "확인하겠습니다.";
-                DescriptionText.text = name + " 분대장님 맞으십니까?";
+                DescriptionText.text = name + " 분대장님 맞으신가요?";
 
-                PositiveButtonText.text = "어 맞아";
-                NegativeButtonText.text = "아니";
+                PositiveButtonText.text = "예";
+                NegativeButtonText.text = "아니오";
                 break;
             case 3:
-                TitleText.text = "전역일 언제십니까?";
-                DescriptionText.text = "디데이 세드리려고 합니다. 전역일 여쭤봐도 되겠습니까?";
+                TitleText.text = "전역일을 알려주세요.";
+                DescriptionText.text = "디데이 세드리려고 합니다 ;)";
                 PositiveButtonText.text = "언제냐면..";
-                NegativeButtonText.text = "이름 잘 못 말했다";
+                NegativeButtonText.text = "뒤로";
                 break;
         }
     }
