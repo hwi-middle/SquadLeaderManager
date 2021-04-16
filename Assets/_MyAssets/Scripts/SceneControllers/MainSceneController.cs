6871c5f72f59a53853f537117c975aebdd9d40f7
hwi-middle <gnlwnd5084@gmail.com>
2021-04-17 00:24:43 +0900

    메인 씬 캔버스 전환 구현 및 레이아웃 수정

--- Assets/_MyAssets/Scripts/SceneControllers/MainSceneController.cs
+++ Assets/_MyAssets/Scripts/SceneControllers/MainSceneController.cs
@@ -5,7 +5,12 @@ using UnityEngine.UI;
 
 public class MainSceneController : MonoBehaviour
 {
+    public Canvas[] canvasArr = new Canvas[5];  //하단바 아이콘 순서대로 저장된 배열
+    public GameObject[] iconArr = new GameObject[5];
+    private int curIdx = 0;
+
     public Text nameText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +21,20 @@ public class MainSceneController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnBottomBarBtnClick(int newIdx)
+    {
+        if (curIdx == newIdx)
+        {
+            ScrollRect scrollRect = canvasArr[curIdx].transform.Find("Main Scroll View").GetComponent<ScrollRect>();
+            scrollRect.normalizedPosition = new Vector2(0, 1);
+        }
+        else
+        {
+            canvasArr[curIdx].gameObject.SetActive(false);
+            canvasArr[newIdx].gameObject.SetActive(true);
+            curIdx = newIdx;
+        }
     }
 }
