657a924a8d4cbd414b1f6a8e966db7e399fd300f
hwi-middle <gnlwnd5084@gmail.com>
2021-04-16 21:07:45 +0900

    메인 씬에 사용자 이름이 적용되도록 변경

--- Assets/_MyAssets/Scripts/SceneControllers/MainSceneController.cs
+++ Assets/_MyAssets/Scripts/SceneControllers/MainSceneController.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainSceneController : MonoBehaviour
 {
+    public Text nameText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        nameText.text = PlayerPrefs.GetString("UserName");
     }
 
     // Update is called once per frame
