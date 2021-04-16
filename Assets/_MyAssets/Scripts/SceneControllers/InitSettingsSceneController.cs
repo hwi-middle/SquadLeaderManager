5f31a07808ab5cb388cffb6ae9ce2d0d854fb425
hwi-middle <gnlwnd5084@gmail.com>
2021-04-16 21:07:59 +0900

    네이밍 컨벤션을 따르도록 변경

--- Assets/_MyAssets/Scripts/SceneControllers/InitSettingsSceneController.cs
+++ Assets/_MyAssets/Scripts/SceneControllers/InitSettingsSceneController.cs
@@ -5,21 +5,21 @@ using UnityEngine.UI;
 
 public class InitSettingsSceneController : MonoBehaviour
 {
-    public Text TitleText;
-    public Text DescriptionText;
-    public InputField TextInputField;
-    public Button PositiveButton;
-    public Button NegativeButton;
-    private Text PositiveButtonText;
-    private Text NegativeButtonText;
+    public Text titleText;
+    public Text descriptionText;
+    public InputField textInputField;
+    public Button positiveButton;
+    public Button negativeButton;
+    private Text positiveButtonText;
+    private Text negativeButtonText;
 
     private int DialogPage = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        PositiveButtonText = PositiveButton.transform.GetChild(0).GetComponent<Text>();
-        NegativeButtonText = NegativeButton.transform.GetChild(0).GetComponent<Text>();
+        positiveButtonText = positiveButton.transform.GetChild(0).GetComponent<Text>();
+        negativeButtonText = negativeButton.transform.GetChild(0).GetComponent<Text>();
         SetTitleAndDescriptionText();
     }
 
@@ -46,45 +46,45 @@ public class InitSettingsSceneController : MonoBehaviour
         switch (DialogPage)
         {
             case 0:
-                TextInputField.gameObject.SetActive(false);
-                NegativeButton.gameObject.SetActive(false);
+                textInputField.gameObject.SetActive(false);
+                negativeButton.gameObject.SetActive(false);
 
-                TitleText.text = "안녕하세요!";
-                DescriptionText.text = "지금부터 초기 설정을 시작하겠습니다.";
-                PositiveButtonText.text = "다음";
+                titleText.text = "안녕하세요!";
+                descriptionText.text = "지금부터 초기 설정을 시작하겠습니다.";
+                positiveButtonText.text = "다음";
                 break;
             case 1:
-                TextInputField.gameObject.SetActive(true);
-                NegativeButton.gameObject.SetActive(true);
+                textInputField.gameObject.SetActive(true);
+                negativeButton.gameObject.SetActive(true);
 
-                TitleText.text = "성함을 알려주세요.";
-                DescriptionText.text = "4글자까지 입력할 수 있습니다.";
-                PositiveButtonText.text = "확인";
-                NegativeButtonText.text = "뒤로";
+                titleText.text = "성함을 알려주세요.";
+                descriptionText.text = "4글자까지 입력할 수 있습니다.";
+                positiveButtonText.text = "확인";
+                negativeButtonText.text = "뒤로";
                 break;
             case 2:
-                string name = TextInputField.text;
+                string name = textInputField.text;
                 PlayerPrefs.SetString("UserName", name);
                 if (name == "")
                 {
-                    TitleText.text = "다시 입력해주세요.";
-                    DescriptionText.text = "이름은 비워둘 수 없습니다.";
+                    titleText.text = "다시 입력해주세요.";
+                    descriptionText.text = "이름은 비워둘 수 없습니다.";
                     DialogPage--;
                     break;
                 }
 
-                TextInputField.gameObject.SetActive(false);
-                TitleText.text = "확인하겠습니다.";
-                DescriptionText.text = name + " 분대장님 맞으신가요?";
+                textInputField.gameObject.SetActive(false);
+                titleText.text = "확인하겠습니다.";
+                descriptionText.text = name + " 분대장님 맞으신가요?";
 
-                PositiveButtonText.text = "예";
-                NegativeButtonText.text = "아니오";
+                positiveButtonText.text = "예";
+                negativeButtonText.text = "아니오";
                 break;
             case 3:
-                TitleText.text = "전역일을 알려주세요.";
-                DescriptionText.text = "디데이 세드리려고 합니다 ;)";
-                PositiveButtonText.text = "언제냐면..";
-                NegativeButtonText.text = "뒤로";
+                titleText.text = "전역일을 알려주세요.";
+                descriptionText.text = "디데이 세드리려고 합니다 ;)";
+                positiveButtonText.text = "언제냐면..";
+                negativeButtonText.text = "뒤로";
                 break;
         }
     }
